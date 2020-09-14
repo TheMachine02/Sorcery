96c5627b5432ca243130fa9ae7362bad2336949e
TheMachine02 <>
2020-09-15 00:31:04 +0200

    Generate sha256 for kernel build

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#generate config
 str=$(git rev-parse --short HEAD)
 echo "; auto generated file" > config
 echo "define CONFIG_KERNEL_NAME \"Sorcery.0.0.1-slp-"$str"\"" >> config
 cat config_build >> config
+echo "Generating initramfs..."
 ./fasmg kernel/bss.asm kernel/initramfs
-lz4 -f -l --best kernel/initramfs initramfs
-rm kernel/initramfs
+lz4 -f -l --rm --best kernel/initramfs initramfs
+echo "Generating initial root..."
+genromfs -d root -V root -f rootfs
+echo "Building kernel..."
+# build kernel
 ./fasmg sorcery.asm SORCERY.8xp
+sha256sum -b SORCERY.8xp | sed 's/ \*.*//'> sha256
+truncate sha256 -s -1
