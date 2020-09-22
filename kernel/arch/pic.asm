d1f6b180b198322ddf4deddf1395a4554861b083
TheMachine02 <>
2020-09-22 15:57:02 +0200

    More helpers

--- kernel/arch/pic.asm
+++ kernel/arch/pic.asm
@@ -1,7 +1,7 @@
 ; RAM reallocated routine for PIC code in library and user process and most essentially XIP
 ; see doc in doc/pic
 
-; swap interrupt and all shadow in 12 cycles
+; swap interrupt and all shadow in 3F
 macro	exxi
 	if defined SWAP
 		ex	af, af'
@@ -12,9 +12,11 @@ macro	exxi
 		di
 		exx
 		ex	af, af'
-	define SWAP 1
+	define SWAP
 	end if
 end	macro
+
+; extern symbol call. Expect bc' as the base data adress, given negative offset
 
 ; call [cc,] (plt+offset)
 __sorcery_extern_call_cc:
@@ -33,6 +35,18 @@ __sorcery_extern_call_cc:
 	exxi
 	ret
 
+; jp [cc,] (plt+offset)
+__sorcery_extern_jp_cc:
+	exxi
+	pop	hl
+	ld	hl, (hl)
+	add	hl, bc
+	push	hl
+	exxi
+	ret
+	
+; In section call and jp. For intersection or library call, see plt
+	
 ; call [cc,] (pc+offset)
 __sorcery_relative_call_cc:
 	exxi
@@ -58,7 +72,7 @@ __sorcery_relative_jp_cc:
 	exxi
 	ret
 	
-; C optimized version wich does framesetting in same time (and destroy for that both hl & ix)
+; C optimized version wich does framesetting in same time (and destroy for that both hl & ix & de)
 ; you need to pop ix before ret in the calledd routine, as per C standard
 ; call frameset0 is a NOP, call _frameset should do add hl,sp / ld sp, hl
 __sorcery_extern_call_C:
@@ -87,12 +101,32 @@ __sorcery_relative_call_C:
 ; push ix back
 	push	hl
 ; now ix and hl are free
-	ex	de, hl
 ; get the offset and add it to the pc
 	ld	de, (ix+0)
 	add	ix, de
-	ex	de, hl
 	lea	hl, ix+0
 	ld	ix, 0
 	add	ix, sp
 	jp	(hl)
+
+; jump to a C function, with ix framesetting
+; de, hl, ix can be destroyed
+__sorcery_extern_jp_C:
+	ex	(sp), ix
+	ld	ix, (ix+0)
+	exx
+	add	ix, bc
+	exx
+	lea	hl, ix+0
+	ld	ix, 0
+	add	ix, sp
+	jp	(hl)
+	
+__sorcery_relative_jp_C:
+	ex	(sp), ix
+	ld	de, (ix+0)
+	add	hl, de
+	ld	ix, 0
+	add	ix, sp
+	jp	(hl)
+	
