241b5c07b06f4357c763402f384b9ea8f2cc9af6
TheMachine02 <>
2020-10-10 09:56:54 +0200

    Point release 0.1.5. Build can also take version as an argument

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #generate config
+version=${1:-1.5.0}
 str=$(git rev-parse --short HEAD)
 echo "; auto generated file" > config
-echo "define CONFIG_KERNEL_VERSION \"0.1.4-slp-"$str"\"" >> config
+echo "define CONFIG_KERNEL_VERSION \""$version"-slp-"$str"\"" >> config
 echo "define CONFIG_KERNEL_RELEASE \"slp-"$str"\"" >> config
 cat build_config >> config
 echo "Generating initramfs..."
@@ -13,15 +14,16 @@ rm kernel/initramfs
 # genromfs -d root -V root -f rootfs
 echo "Building kernel..."
 # build kernel
-fasmg sorcery.asm SORCERY.8xp
-fasmg vm.asm VMLOADER.8xp
-fasmg vmsorcerz.asm sorcery-0.1.4-slp
+fasmg sorcery.asm bin/SORCERY.8xp
+fasmg vm.asm bin/VMLOADER.8xp
+fasmg vmsorcerz.asm bin/sorcery-$version-slp
 echo "include	'header/include/ez80.inc'" > image.asm
 echo "include	'header/include/tiformat.inc'" >> image.asm
 echo "format	ti executable 'VMSCZ'" >> image.asm
-echo "file	'sorcery-0.1.4-slp'" >> image.asm
-fasmg image.asm VMSCZ.8xp
+echo "file	'bin/sorcery-"$version"-slp'" >> image.asm
+fasmg image.asm bin/VMSCZ.8xp
 rm image.asm
+rm initramfs
 
-sha256sum -b sorcery-0.1.4-slp | sed 's/ \*.*//'> sha256
+sha256sum -b bin/sorcery-$version-slp | sed 's/ \*.*//'> bin/sha256
 truncate sha256 -s -1
