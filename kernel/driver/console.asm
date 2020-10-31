722284a900435f085da4d7e93304c427bf98347c
grosged <66749840+grosged@users.noreply.github.com>
2020-10-31 16:38:56 +0100

    Update console.asm

--- kernel/driver/console.asm
+++ kernel/driver/console.asm
@@ -399,39 +399,26 @@ console:
 ; de = address
 	ld	hl, 40
 	add	hl, de
-	ex	de, hl
-	push	de
+	push	hl
+	ld	de, 4
 	ld	a, 2
 	ld	c, 8
 .color_block_b:
 	ld	b, 24
 .color_block:
-	ld	(de), a
-	inc	de
+	ld	(hl), a
+	inc	hl
 	djnz	.color_block
 	inc	a
+	add	hl, de
 	dec	c
-	inc	de
-	inc	de
-	inc	de
-	inc	de
 	jr	nz, .color_block_b
 	pop	de
 	ld	hl, 320
 	add	hl, de
 	ex	de, hl
-	ld	a, 10
-	ld	c,220
-.color_copy_block:
+	ld	bc,(320*9)+220
 	ldir
-	ld	c, 100
-	ex	de, hl
-	add	hl, bc
-	ex	de, hl
-	ld	c,220
-	sbc	hl, bc
-	dec	a
-	jr	nz, .color_copy_block
 	ld	iy, console_dev
 	call	.phy_new_line
 	jp	.prompt
