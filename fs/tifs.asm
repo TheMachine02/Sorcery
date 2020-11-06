846302df5c000642b9d5c06b6d92dd0015c00eb1
grosged <66749840+grosged@users.noreply.github.com>
2020-11-06 14:55:42 +0100

    Update tifs.asm

--- fs/tifs.asm
+++ fs/tifs.asm
@@ -52,15 +52,11 @@ tifs:
 ; marking the variable as removed in flash
 ; iy = inode
 	ld	ix, (iy+KERNEL_VFS_INODE_DMA_DATA)
-	ld	de, (ix+KERNEL_VFS_INODE_DMA_POINTER)
-; hl is pointer to flash memory
+	ld	ix, (ix+KERNEL_VFS_INODE_DMA_POINTER)
+; ix is pointer to flash memory
 ; back search the begin of the variable
-	dec	de
-	dec	de
-	dec	de
-	ld	hl, -6
-	add	hl, de
-	ex	de, hl
+	lea	hl, ix-3
+	lea	de, ix-9
 .field_search:
 ; search the TIFS_FILE_ADRESS field by checking if de = bc
 	ld	bc, (hl)
