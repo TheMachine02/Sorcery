8e49fbed30dce9fd0c6b0d32ff3c7cca06c1c651
TheMachine02 <>
2022-07-14 23:07:23 +0200

    hypervisor: small changes for exec_static

--- hypervisor/boot.asm
+++ hypervisor/boot.asm
@@ -29,28 +29,23 @@ leaf:
 	ret
 
 .exec_static:
-; grab the entry point of the program and jump to it
-; make section protected for the kernel ?
-; execute in place
-; we need to reallocate here
-; read section table and copy at correct location (for those needed)
 	lea	bc, iy+0
 	ld	ix, (iy+LEAF_HEADER_SHOFF)
 	add	ix, bc
 ; read section now
 	ld	b, (iy+LEAF_HEADER_SHNUM)
-.alloc_prog_loop:
+.alloc_prog_section:
 	bit	1, (ix+LEAF_SECTION_FLAGS)
 	jr	z, .alloc_next_section
 	push	bc
 	ld	hl, $E40000+SHT_NOBITS
 	ld	a, (ix+LEAF_SECTION_TYPE)
 	cp	a, l
-	jr	z, .copy_null
+	jr	z, .alloc_nobits
 	ld	hl, (ix+LEAF_SECTION_OFFSET)
 	lea	bc, iy+0
 	add	hl, bc
-.copy_null:
+.alloc_nobits:
 	ld	bc, (ix+LEAF_SECTION_SIZE)
 ; we are a static file, the addr is RAM adress
 	ld	de, (ix+LEAF_SECTION_ADDR)
@@ -58,7 +53,7 @@ leaf:
 	pop	bc
 .alloc_next_section:
 	lea	ix, ix+16
-	djnz    .alloc_prog_loop
+	djnz    .alloc_prog_section
 	bit	LF_PROTECTED_BIT, (iy+LEAF_HEADER_FLAGS)
 	call	nz, .protected_static
 ; load up entry
@@ -76,27 +71,28 @@ leaf:
 	add	ix, bc
 ; read section now
 	ld	b, (iy+LEAF_HEADER_SHNUM)
-.bound_loop:
+.protected_boundary:
 	bit	1, (ix+LEAF_SECTION_FLAGS)
-	jr	z, .bound_next_section
+	jr	z, .protected_next_section
 	ld	de, (ix+LEAF_SECTION_ADDR)
 	ld	hl, (leaf_bound_lower)
+	or	a, a
 	sbc	hl, de
-	jr	c, .bound_upper
+	jr	c, .protected_bound_upper
 	ld	(leaf_bound_lower), de
-.bound_upper:
+.protected_bound_upper:
 	ld	hl, (ix+LEAF_SECTION_SIZE)
 	add	hl, de
 	ex	de, hl
 	ld	hl, (leaf_bound_upper)
 	or	a, a
 	sbc	hl, de
-	jr	nc, .bound_lower
+	jr	nc, .protected_bound_lower
 	ld	(leaf_bound_upper), de
-.bound_lower:
-.bound_next_section:
+.protected_bound_lower:
+.protected_next_section:
 	lea	ix, ix+16
-	djnz	.bound_loop
+	djnz	.protected_boundary
 	ld	hl, leaf_bound_lower
 	ld	bc, $620
 	otimr
